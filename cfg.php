11d4b7242b852e5633a87256f6b87aa1570480f8
Dava Firmansyah <sindivegeance@gmail.com>
2021-10-22 23:30:43 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,6 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.114 Mobile Safari/537.36";
+
+$cookie = "_ym_uid=163360957380019580; _ym_d=1633609573; _ym_isad=2; csrf_cookie_name=3107dfb156a1d1e630a6ea3afcb65b54; ci_session=gciht0bg4konha262ught76ae9nfa6gb";
+
